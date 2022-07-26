f7030b1396c0ac43528709ebb4cf459ea924fdf6
Scrappyz <94944632+Scrappyz@users.noreply.github.com>
2022-07-26 17:37:07 +0800

    Update for cmd

--- main.hpp
+++ main.hpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <sstream>
 #include <cstring>
-//#include <conio.h
 
 #define MAX_SCRAMBLED_SIZE 24
 #define MAJOR_VER 0
@@ -17,10 +16,22 @@ struct dict{
   dict();
 };
 
+struct Functions {
+    static std::vector<std::string> param_list;
+    bool help;
+    bool substring;
+
+    Functions() : help(), substring() {}
+
+    void print_help();
+    bool valid_args(const std::vector<std::string>& args);
+    void set_functions(const std::vector<std::string>& args);
+};
+
 int get_index(const char &c);
 dict process_word(const std::string &here);
 bool check_if_pass(const dict &source, dict &current);
 void load_settings(std::ifstream &settings, std::string &dictionary_file);
-void preload_dictionary(const std::string& dictionary_file);
-void scramble(const std::string &dictionary_file);
+void preload_dictionary(const std::string& dictionary_file, bool message = true);
+void scramble(const std::string &dictionary_file, bool substring = false);
 void change_dictionary(std::string &dictionary_file);
